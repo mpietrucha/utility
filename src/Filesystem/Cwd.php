007ffe6aa4f0e230813d5e4e6eed2bf9ad358225
Michał Pietrucha <mpietrucha50@gmail.com>
2025-10-22 22:58:36 +0200

    wip

--- src/Filesystem/Cwd.php
+++ src/Filesystem/Cwd.php
@@ -3,11 +3,26 @@
 namespace Mpietrucha\Utility\Filesystem;
 
 use Mpietrucha\Utility\Filesystem;
+use Mpietrucha\Utility\Utilizer\Concerns\Utilizable;
+use Mpietrucha\Utility\Utilizer\Contracts\UtilizableInterface;
 
-abstract class Cwd
+abstract class Cwd implements UtilizableInterface
 {
+    use Utilizable\Strings;
+
     public static function get(): string
     {
-        return Filesystem::cwd();
+        return static::utilize();
+    }
+
+    protected static function hydrate(): string
+    {
+        $cwd = Filesystem::cwd();
+
+        if (Filesystem::exists($cwd, 'composer.json')) {
+            return $cwd;
+        }
+
+        return Path::directory($cwd);
     }
 }
