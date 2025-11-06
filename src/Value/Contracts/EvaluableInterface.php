4800197e3bff047d991308fe256767e11b9b5e4a
Michał Pietrucha <mpietrucha50@gmail.com>
2025-11-06 18:33:52 +0100

    wip

--- src/Value/Contracts/EvaluableInterface.php
+++ src/Value/Contracts/EvaluableInterface.php
@@ -13,13 +13,21 @@ interface EvaluableInterface
      * Determine whether the evaluable is a valid callable.
      *
      * @phpstan-assert-if-true callable $this->evaluable()
+     * @phpstan-assert-if-true false $this->unsupported()
+     *
+     * @phpstan-assert-if-false null $this->evaluable()
+     * @phpstan-assert-if-false true $this->unsupported()
      */
     public function supported(): bool;
 
     /**
      * Determine whether the evaluable is not a valid callable.
      *
+     * @phpstan-assert-if-true null $this->evaluable()
+     * @phpstan-assert-if-true false $this->supported()
+     *
      * @phpstan-assert-if-false callable $this->evaluable()
+     * @phpstan-assert-if-false true $this->supported()
      */
     public function unsupported(): bool;
 
