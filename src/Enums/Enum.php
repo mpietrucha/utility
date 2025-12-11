04a60c7e67183c4d75e9d85cf567fe1e0a6ab632
Michał Pietrucha <mpietrucha50@gmail.com>
2025-12-11 10:48:49 +0100

    wip

---
+++ src/Enums/Enum.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Mpietrucha\Utility\Enums;
+
+use Mpietrucha\Utility\Concerns\Compatible;
+use Mpietrucha\Utility\Contracts\CompatibleInterface;
+use Mpietrucha\Utility\Enums\Contracts\InteractsWithEnumInterface;
+use Mpietrucha\Utility\Instance;
+use Mpietrucha\Utility\Value;
+
+abstract class Enum implements CompatibleInterface
+{
+    use Compatible;
+
+    public static function contract(): string
+    {
+        return InteractsWithEnumInterface::class;
+    }
+
+    protected static function compatibility(mixed $input): bool
+    {
+        $evaluation = Instance::is(...) |> Value::attempt(...);
+
+        return $evaluation->eval([
+            $input,
+            static::contract(),
+        ])->boolean();
+    }
+}
