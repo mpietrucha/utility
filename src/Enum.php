504d60bd702f27517200e176502a70b7ff846d84
Michał Pietrucha <mpietrucha50@gmail.com>
2025-12-18 13:46:17 +0100

    wip

--- src/Enum.php
+++ src/Enum.php
@@ -10,6 +10,9 @@ abstract class Enum implements CompatibleInterface
 {
     use Compatible;
 
+    /**
+     * @phpstan-assert-if-true class-string<\Mpietrucha\Utility\Enums\Contracts\InteractsWithEnumInterface> $input
+     */
     protected static function compatibility(mixed $input): bool
     {
         $evaluation = Instance::is(...) |> Value::attempt(...);
